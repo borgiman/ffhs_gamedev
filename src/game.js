08ff53655aaf3fa893f1c2f5ee903a9786e7f266
Patrick Borgogno <789401+borgiman@users.noreply.github.com>
2023-10-01 20:01:41 +0200

    create canvas and list upcoming todos

--- src/game.js
+++ src/game.js
@@ -1,4 +1,18 @@
-const paragraphElement = document.createElement('p');
-const textNode = document.createTextNode('this paragraph was created in code');
-paragraphElement.appendChild(textNode);
-document.body.appendChild(paragraphElement);
+/*
+    todo patrick
+    - display something on every click in the canvas
+    - display that something on the position of the mouse
+    - use a defense tower sprite as that something that gets displayed
+    - draw a circle with hardcoded radius around that sprite
+    - detect when the mouse is inside the circle and log something
+    - log only at most once every 2s
+    - instead of logging draw a rocket sprite on the position of the defense tower
+    - move that sprite a bit on every tick towards to the location where the mouse was detected
+    - turn the sprite so that the rotation matches the way it is moving
+    - when the target location is reached remove rocket sprite and replace with static explosion sprite
+    - remove static explosion sprite after 1s
+    - use the current location of the mouse as target of the rocket
+    - use last known position inside the circle of the mouse when it is outside for the target of the rocket
+    - play a sound when the rocket is flying
+    - play a sound on impact
+ */
